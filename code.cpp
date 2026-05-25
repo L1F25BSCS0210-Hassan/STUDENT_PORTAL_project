a7716c0cabc069a4375848e6e9f0db233350c519
L1F25BSCS0210-Hassan <hz4055225@gmail.com>
2026-05-25 23:28:08 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,57 +4,46 @@
 using namespace std;
 
 void addStudent(int &count,char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[]);
-void showStudent(int count,char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[])
-{
-    if(count==0)
-    {
-        cout<<"No student data to show...\n";
-        return;
-    }
-    
-    cout<<"===================STUDENT DATA====================";
-    for(int i=0;i<count;i++)
-    {
-        cout << "\nStudent " << i + 1 << ":\n";
-        cout<<fixed;
-        cout.width(20);
-
-        cout<<left<<"Student's name ";
-        cout<<" : ";
-        
-        cout<<NAME[i]<<endl;
-        cout.width(20);
-        cout<<left<<"Student's Department : ";
-        
-        cout<<DEPT[i]<<endl;
-        cout.width(20);
-        cout<<left<<"Student's ID ";
-        
-        cout<<" : ";
-        
-        cout<<ID[i]<<endl;
-        cout.width(20);
-        cout<<left<<"Student's CGPA ";
-        
-        cout<<" : ";
-        cout.precision(2);
-        cout<<CGPA[i]<<endl;
-        cout<<endl;
-    }
-    cout<<"===================================================";
-    cout<<endl;
-}
+void showStudent(int count,char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[]);
+void searchStudent(int choice,int &count,char searchNAME[],char searchID[],char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[]);
+void deptFilter(int &count,char searchDEPT[],char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[]);
 
 int main()
 {
     char NAME[25][30];
+    char searchNAME[30];
     char DEPT[25][25];
+    char searchDEPT[25];
     char ID[25][25];
+    char searchID[25];
     float CGPA[50];
     int count=0;
+    int choice;
     
+    cout<<"Enter 1 to search by NAME...\nEnter 2 to search by ID....\n";
+    cin>>choice;
+    if(choice==1)
+    {
+        cout<<"Enter the student's NAME you want to search...\n";
+        cin.getline(searchNAME,30);
+    }
+    else if(choice==2)
+    {
+        cout<<"Enter the student's ID you want to search...\n";
+        cin.getline(searchNAME,30);
+    }
+    else
+    {
+        cout<<"Invalid Choice..\n";
+    }
+    
+    
+    cout<<"Enter the department according to which you want to filter students: \n";
+    cin.getline(searchDEPT,25);
     addStudent(count,NAME,DEPT,ID,CGPA);
     showStudent(count,NAME,DEPT,ID,CGPA);
+    searchStudent(choice,count,searchNAME,searchID,NAME,DEPT,ID,CGPA);
+    deptFilter(count,searchDEPT,NAME,DEPT,ID,CGPA);
     return 0;
 }
 
@@ -106,3 +95,152 @@ void addStudent(int &count,char NAME[][30],char DEPT[][25],char ID[][25],float C
     count++;
 }
 
+
+void showStudent(int count,char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[])
+{
+    if(count==0)
+    {
+        cout<<"No student data to show...\n";
+        return;
+    }
+    
+    cout<<"===================STUDENT DATA====================";
+    for(int i=0;i<count;i++)
+    {
+        cout << "\nStudent " << i + 1 << ":\n";
+        cout<<fixed;
+        cout.width(20);
+
+        cout<<left<<"Student's name ";
+        cout<<" : ";
+        
+        cout<<NAME[i]<<endl;
+        cout.width(20);
+        cout<<left<<"Student's Department : ";
+        
+        cout<<DEPT[i]<<endl;
+        cout.width(20);
+        cout<<left<<"Student's ID ";
+        
+        cout<<" : ";
+        
+        cout<<ID[i]<<endl;
+        cout.width(20);
+        cout<<left<<"Student's CGPA ";
+        
+        cout<<" : ";
+        cout.precision(2);
+        cout<<CGPA[i]<<endl;
+        cout<<endl;
+    }
+    cout<<"===================================================";
+    cout<<endl;
+}
+
+void searchStudent(int choice,int &count,char searchNAME[],char searchID[],char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[])
+{
+    if(choice==1)
+    {
+        for(int i=0;i<count;i++)
+        {
+            if(strcmp(searchNAME,NAME[i])==0)
+            {
+                cout<<fixed;
+                cout.width(20);
+        
+                cout<<left<<"Student's name ";
+                cout<<" : ";
+                
+                cout<<NAME[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's Department : ";
+                
+                cout<<DEPT[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's ID ";
+                
+                cout<<" : ";
+                
+                cout<<ID[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's CGPA ";
+                
+                cout<<" : ";
+                cout.precision(2);
+                cout<<CGPA[i]<<endl;
+            }
+        }
+    }
+    
+    else if(choice==2)
+    {
+        
+        for(int i=0;i<count;i++)
+        {
+            if(strcmp(searchID,ID[i])==0)
+            {
+                cout<<fixed;
+                cout.width(20);
+        
+                cout<<left<<"Student's name ";
+                cout<<" : ";
+                
+                cout<<NAME[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's Department : ";
+                
+                cout<<DEPT[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's ID ";
+                
+                cout<<" : ";
+                
+                cout<<ID[i]<<endl;
+                cout.width(20);
+                cout<<left<<"Student's CGPA ";
+                
+                cout<<" : ";
+                cout.precision(2);
+                cout<<CGPA[i]<<endl;
+            }
+        }
+    }
+    else
+    {
+        cout<<"INVALID CHOICE...\n";
+    }
+}
+
+
+void deptFilter(int &count,char searchDEPT[],char NAME[][30],char DEPT[][25],char ID[][25],float CGPA[])
+{
+    for(int i=0;i<count;i++)
+    {
+        if(strcmp(searchDEPT,DEPT[i])==0)
+        {
+            cout<<fixed;
+            cout.width(20);
+        
+            cout<<left<<"Student's name ";
+            cout<<" : ";
+                
+            cout<<NAME[i]<<endl;
+            cout.width(20);
+            cout<<left<<"Student's Department : ";
+                
+            cout<<DEPT[i]<<endl;
+            cout.width(20);
+            cout<<left<<"Student's ID ";
+                
+            cout<<" : ";
+                
+            cout<<ID[i]<<endl;
+            cout.width(20);
+            cout<<left<<"Student's CGPA ";
+                
+            cout<<" : ";
+            cout.precision(2);
+            cout<<CGPA[i]<<endl;
+        }
+    }     
+}
