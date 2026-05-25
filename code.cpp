a9d2ec98bafcafaa4f4f21a9eed601019e98bc47
L1F25BSCS0210-Hassan <hz4055225@gmail.com>
2026-05-25 21:09:15 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+using namespace std;
+
+void addStudent(int &count,char NAME[][30],char DEPT[][25],char &ID[][25],float &CGPA[]);
+
+int main()
+{
+    char NAME[25][30];
+    char DEPT[25][25];
+    char ID[25][25];
+    float CGPA[50];
+    int count=0;
+    
+    
+    addStudent(count,NAME,DEPT,ID,CGPA);
+    return 0;
+}
+
+
+void addStudent(int &count,char NAME[][30],char DEPT[][25],char &ID[][25],float &CGPA[])
+{
+    char name[50];
+    char dept[30];
+    int id;
+    float cgpa;
+    int i=0;
+    
+    cout<<"Enter the student name:\n";
+    cin.getline(name,50);
+    
+    while(name[i]!='\0')
+    {
+        NAME[count][i]=name[i];
+        i++;
+    }
+    
+    NAME[count][i]='\0';
+    
+    i=0;
+    cout<<"Enter student's department:\n";
+    cin.getline(dept,30);
+    
+    while(dept[i]!='\0')
+    {
+        DEPT[count][i]=dept[i];
+        i++;
+    }
+    DEPT[i]='\0';
+    
+    i=0;
+    cout<<"Enter student's ID:\n";
+    cin>>id;
+    
+    ID[count]=id;
+    cout<<"Enter student's CGPA: \n";
+    cin>>cgpa;
+    CGPA[count]=cgpa;
+    
+    count++;
+}
